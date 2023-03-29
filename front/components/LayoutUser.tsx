76b84485c0e3b223731df2dbdd788a4ab25c156d
Mel.R <mel.redureau@gmail.com>
2023-03-29 16:45:32 +0200

    Change the burger menu icon when clicked

--- front/components/LayoutUser.tsx
+++ front/components/LayoutUser.tsx
@@ -14,25 +14,34 @@ const LayoutUser = ({ children }: LayoutProps) => {
         }}
         className="grid gap-8 px-4  items-center grid-cols-menu-mobile sticky  w-full shadow-neutral-150  shadow-lg "
       >
-        <div
-          style={{ gridArea: "burger" }}
-          className="space-y-1.5"
-          onClick={() => setIsMenuOpened((prevState) => !prevState)}
-        >
+
+          <div style={{ gridArea: "burger" }} className={`${isMenuOpened? "h-5 mt-0":""} space-y-1.5 w-fit`} onClick={() => setIsMenuOpened((prevState) => !prevState)}>
+    {isMenuOpened ? (
+      <>
+          <div className="w-6 h-0.5 bg-gray-600 translate-y-2 transform rotate-45"></div>
+          <div className="w-6 h-0.5 bg-gray-600 transform -rotate-45"></div>
+      </>
+          ):( 
+      <>
           <div className="w-6 h-0.5 bg-gray-600"></div>
           <div className="w-6 h-0.5 bg-gray-600"></div>
           <div className="w-6 h-0.5 bg-gray-600"></div>
+      </>
+
+)}
         </div>
+
+        
         <img
           src="/jaji.svg"
           alt=""
           style={{ gridArea: "logo" }}
-          className=""
+          className="py-4"
         />
         <div className=""></div>
       </div>
       {isMenuOpened && (
-        <div className="absolute mt-10 py-8 z-10 w-screen top-0 left-0 bg-white flex flex-col gap-2 text-blue-350 rounded-lg shadow-md">
+        <div className="absolute mt-16 py-8 z-10 w-screen top-0 left-0 bg-white flex flex-col gap-2 text-blue-350 rounded-lg shadow-md">
           <Link className="w-fit m-auto hover:text-blue-300" href="/">Content</Link>
           <Link className="w-fit m-auto hover:text-blue-300" href="/">Log Out</Link>
         </div>
