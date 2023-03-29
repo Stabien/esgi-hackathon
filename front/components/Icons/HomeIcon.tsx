3794861d2f9b819d809441af633843167d3c4116
Mel.R <mel.redureau@gmail.com>
2023-03-29 15:43:47 +0200

    Fix issues on icons

--- front/components/Icons/HomeIcon.tsx
+++ front/components/Icons/HomeIcon.tsx
@@ -6,10 +6,10 @@ const HomeIcon = ({ size = "20", color = "currentColor" }) => {
 width={size} height={size} viewBox="0 0 16 17" 
 fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_80_29)">
-<path d="M14.02 6.67L7.51 1L1 6.67" stroke="#00224B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M2.45001 15.18V8.88" stroke="#00224B" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
-<path d="M12.57 15.18V8.88" stroke="#00224B" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
-<path d="M2.45001 15.18H12.57" stroke="#00224B" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
+<path d="M14.02 6.67L7.51 1L1 6.67" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2.45001 15.18V8.88" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
+<path d="M12.57 15.18V8.88" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
+<path d="M2.45001 15.18H12.57" stroke={color} stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"/>
 </g>
 <defs>
 <clipPath id="clip0_80_29">
