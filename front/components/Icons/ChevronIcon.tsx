75b4ccfa527f3bfa936c51ea9718cf518701dfe6
Mel.R <mel.redureau@gmail.com>
2023-03-30 16:37:02 +0200

    Change chevron Icon and chevron icon to site menu

--- front/components/Icons/ChevronIcon.tsx
+++ front/components/Icons/ChevronIcon.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const ChevronIcon = ({ size = "20", color = "currentColor", rotation = "0deg"}) => {
+const ChevronIcon = ({ size = "20", color = "currentColor", rotation = 0 }) => {
     return (
-<svg width={size}height={size} viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width={size}height={size} viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg" transform={`rotate(${rotation})`}>
 <path d="M6.13945 8.06641C6.40312 8.33008 6.84258 8.33008 7.10625 8.06641L12.8191 2.38281C13.0828 2.08984 13.0828 1.65039 12.8191 1.38672L12.1453 0.712891C11.8816 0.449219 11.4422 0.449219 11.1492 0.712891L6.6375 5.22461L2.09648 0.712891C1.80352 0.449219 1.36406 0.449219 1.10039 0.712891L0.426562 1.38672C0.16289 1.65039 0.16289 2.08984 0.426562 2.38281L6.13945 8.06641Z" fill={color}/>
 </svg>
         );
--- front/components/SiteMenu.tsx
+++ front/components/SiteMenu.tsx
@@ -1,8 +1,9 @@
 import { selectUser } from "@/redux/user/user.selectors";
 import { User } from "@/types";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import ChevronIcon from "./Icons/ChevronIcon";
 import HomeIcon from "./Icons/HomeIcon";
 import PlusIcon from "./Icons/PlusIcon";
 import StatIcon from "./Icons/StatIcon";
@@ -11,6 +12,9 @@ type Props = {};
 
 function SiteMenu({}: Props) {
   const user: User = useSelector(selectUser);
+  const [isContentMenuOpened, setIsContentMenuOpened] = useState<boolean>(false);
+  const [isStatMenuOpened, setIsStatMenuOpened] = useState<boolean>(false);
+
   return (
     <article style={{ gridArea: "menu" }} className="flex  flex-col box-border sticky left-0 top-0 overflow-y-hidden h-screen px-8 pt-4">
      
@@ -30,9 +34,11 @@ function SiteMenu({}: Props) {
             <div className="text-neutral-500 my-5">
               <div className="flex flew-row gap-5">
                 <div className="my-auto h-fit"><PlusIcon size="20"/> </div>
-                <div className="font-semibold text-2xl">Contenu</div> 
+                <div className="font-semibold text-xl" onClick={()=>setIsContentMenuOpened(!isContentMenuOpened)}>Contenu</div> 
+                {isContentMenuOpened ? (<div className="my-auto h-fit"><ChevronIcon size="10"/></div>):(<div className="my-auto h-fit"><ChevronIcon size="10" rotation={180}/></div> )}
               </div>
-              <div className="flex flex-col ml-10 justify-start">
+              {isContentMenuOpened&&
+               <div className="flex flex-col ml-10 justify-start">
                 <div className="text-sm">
                   <Link href="/new-content">Nouvel article</Link>
                 </div>
@@ -40,13 +46,16 @@ function SiteMenu({}: Props) {
                   <Link href="/content">Tous les contenus</Link>
                 </div>
               </div>
+              }
           </div>
 
             <div className="text-neutral-500 ">
               <div className="flex flew-row gap-5">
                 <div className="my-auto h-fit"><StatIcon size="20"/></div>
-                <div className="font-semibold text-2xl">Client</div> 
+                <div className="font-semibold text-xl" onClick={()=>setIsStatMenuOpened(!isStatMenuOpened)}>Statistiques</div> 
+                {isStatMenuOpened ? (<div className="my-auto h-fit"><ChevronIcon size="10"/></div>):(<div className="my-auto h-fit"><ChevronIcon size="10" rotation={180}/></div> )}
               </div>
+              {isStatMenuOpened&&               
               <div className="flex flex-col ml-10 justify-start">
                 <div className="text-sm">
                 <Link href="/new-content">Campagnes</Link>
@@ -55,6 +64,8 @@ function SiteMenu({}: Props) {
                   <Link href="/content">Tous les contenus</Link>
                 </div>
               </div>
+              }
+
             </div>
             
        </>
