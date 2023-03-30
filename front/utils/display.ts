55020a5327b810445d970e30151c259756a842af
RSelwa <selwa.raphael@gmail.com>
2023-03-30 13:14:13 +0200

    💄 ContentForm

--- front/utils/display.ts
+++ front/utils/display.ts
@@ -5,3 +5,16 @@ export const arrayToSelect = (
   array: string[]
 ): { label: string; value: string }[] =>
   array.map((a) => ({ label: a, value: a }));
+
+export const getRandomBackground = () => {
+  const bgClassesArray = [
+    "bg-neutral-200",
+    "bg-blue-200",
+    "bg-green-200",
+    "bg-yellow-200",
+    "bg-red-200",
+    "bg-pink-200",
+    "bg-purple-200",
+  ];
+  return bgClassesArray[Math.floor(Math.random() * bgClassesArray.length)];
+};
