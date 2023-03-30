be1aea7c312e2af008ac9aaf7cdef466790400a7
RSelwa <selwa.raphael@gmail.com>
2023-03-30 15:39:09 +0200

    💄 contentList

--- front/pages/contents.tsx
+++ front/pages/contents.tsx
@@ -1,3 +1,4 @@
+import ContentList from "@/components/ContentList";
 import { LayoutAdmin } from "@/components/LayoutAdmin";
 import { db } from "@/constants/db";
 import { Content } from "@/types/content";
@@ -8,32 +9,9 @@ import React, { useEffect, useState } from "react";
 type Props = {};
 
 const ContentPage = (props: Props) => {
-  const [contentList, setContentList] = useState<Content[]>([]);
-
-  const fetchContent = async () => {
-    const newContentList: Content[] = [];
-    const result = await getDocs(collection(db, "content"));
-    result.forEach((doc) => {
-      newContentList.push({ ...(doc.data() as Content), uid: doc.id });
-      // doc.data() is never undefined for query doc snapshots
-    });
-    setContentList(newContentList);
-  };
-
-  useEffect(() => {
-    fetchContent();
-  }, []);
-
   return (
     <LayoutAdmin>
-      <div>
-        {contentList.map((content) => (
-          <div key={content.uid}>
-            {content.uid}{" "}
-            <Link href={`/edit-content/${content.uid}`}>edit</Link>{" "}
-          </div>
-        ))}
-      </div>
+      <ContentList />
     </LayoutAdmin>
   );
 };
